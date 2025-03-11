ebcb6419bdc2c264448eb63bd86051f3aff609b5
Nino <tekabu0622@gmail.com>
2025-03-11 17:48:06 +0800

    test serial

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,23 @@
 #include <Arduino.h>
+#include <SoftwareSerial.h>
+
+#define PZEM_TX 2
+#define PZEM_RX 3
+
+SoftwareSerial SoftSerial(PZEM_TX, PZEM_RX);
 
 void setup() {
+  Serial.begin(9600);
+  SoftSerial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  while (Serial.available()) {
+    char c = Serial.read();
+    SoftSerial.write(c);
+  }
+  while (SoftSerial.available()) {
+    char c = SoftSerial.read();
+    Serial.write(c);
+  }
 }
